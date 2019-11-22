95857e1475bd76b221bc53227caba13a3b67875a
shivamsoods <shivam.sood708@gmail.com>
2019-11-22 17:14:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/shivam_constrainted/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.shivam_constrainted;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.animation.AnticipateInterpolator;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    boolean show = false;
+    ConstraintLayout cc1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cc1 = findViewById(R.id.cc1);
+
+        imageView = findViewById(R.id.backgroundImage);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (show)
+                    revertAnimation();
+                else
+                    showAnimation();
+            }
+        });
+
+    }
+
+    private void showAnimation() {
+        show = true;
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(this, R.layout.activity_alt);
+
+        ChangeBounds transition = new ChangeBounds();
+        transition.setInterpolator(new AnticipateInterpolator(1.0f));
+        transition.setDuration(1200);
+
+        TransitionManager.beginDelayedTransition(cc1, transition);
+        constraintSet.applyTo(cc1);
+    }
+
+    private void revertAnimation() {
+        show = false;
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(this, R.layout.activity_main);
+
+        ChangeBounds transition = new ChangeBounds();
+        transition.setInterpolator(new AnticipateInterpolator(1.0f));
+        transition.setDuration(1200);
+
+        TransitionManager.beginDelayedTransition(cc1, transition);
+        constraintSet.applyTo(cc1);
+
+    }
+
+}
